91016f0fac8755993b96c413616716eb7170457b
kheizaran-nazari-khakeshoori <kheizaran-nazari-khakeshoori@gmail.com>
2025-11-03 09:45:31 +0100

    adding scipy version

--- paper1_vrp_general/branch-and-bound-scipy.py
+++ paper1_vrp_general/branch-and-bound-scipy.py
@@ -6,7 +6,7 @@ import matplotlib.patches as mpatches
 
 node_counter = 0
 edges = []
-best_solution = ((None, None), float('-inf'))  # (solution, objective value)
+best_solution = ((None, None), float('-inf')) 
 node_values = {}
 
 
@@ -26,16 +26,16 @@ def branch_and_bound(x1_range=(0, None), x2_range=(0, None)):
 
     if res.success:
         x1_val, x2_val = res.x
-        objective = -res.fun  # Since we are minimizing the negative of the objective function
+        objective = -res.fun  
 
-        # Update node_values with the current node's x, y values, and objective value
+        
         node_values[node_id] = (round(x1_val, 2), round(x2_val, 2), round(objective, 2))
 
-        # Update the best solution if a new better integer solution is found
+       
         if x1_val.is_integer() and x2_val.is_integer() and objective > best_solution[1]:
             best_solution = ((x1_val, x2_val), objective)
 
-        # Branch on x1 if not integer
+   
         if not x1_val.is_integer():
             x1_floor = int(np.floor(x1_val))
             edges.append((node_id, node_counter))
@@ -43,7 +43,7 @@ def branch_and_bound(x1_range=(0, None), x2_range=(0, None)):
             edges.append((node_id, node_counter))
             branch_and_bound(x1_range=(x1_floor+1, None), x2_range=x2_range)
 
-        # Branch on x2 if not integer
+        
         if not x2_val.is_integer():
             x2_floor = int(np.floor(x2_val))
             edges.append((node_id, node_counter))
@@ -58,13 +58,13 @@ def visualize_tree(edges, node_values):
     
     pos = nx.drawing.nx_agraph.graphviz_layout(G, prog='dot')
     
-    # Zeichnen Sie die Knoten (unsichtbar)
+   
     nx.draw_networkx_nodes(G, pos, node_color='white', node_size=1)
     
-    # Zeichnen Sie die Kanten
+    
     nx.draw_networkx_edges(G, pos)
     
-    # Zeichnen Sie die Labels mit einer Bounding-Box um jedes
+   
     labels = {}
     for node, values in node_values.items():
         labels[node] = f"x={values[0]}, y={values[1]}, z={values[2]}"
@@ -79,5 +79,5 @@ branch_and_bound()
 
 print(f"Best integer result: x={best_solution[0][0]}, y={best_solution[0][1]}")
 
-# Visualize the tree
+
 visualize_tree(edges, node_values)
